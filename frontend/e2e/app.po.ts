dff4b4eb0ce2f299c6a9107bfa9b6c1d99010968
offbynull <offbynull@gmaail.com>
2017-12-23 08:39:39 -0800

    Primed Angular frontend projecty

---
+++ frontend/e2e/app.po.ts
@@ -0,0 +1,11 @@
+import { browser, by, element } from 'protractor';
+
+export class AppPage {
+  navigateTo() {
+    return browser.get('/');
+  }
+
+  getParagraphText() {
+    return element(by.css('rfm-root h1')).getText();
+  }
+}
